3ab2bb65c58bcf81645079598861b33e8bbceae1
Fred Baube <fbaube@welho.com>
2020-07-29 23:00:04 +0300

    Refactoring; no more Singleton.

--- xmlcfg.go
+++ xmlcfg.go
@@ -12,63 +12,48 @@ import (
 
 	"errors"
 
-	// XCU "github.com/fbaube/cliflagutils"
 	"github.com/fbaube/db"
 	FU "github.com/fbaube/fileutils"
-	"github.com/fbaube/gparse"
 	MU "github.com/fbaube/miscutils"
 	SU "github.com/fbaube/stringutils"
 	WU "github.com/fbaube/wasmutils"
 	XM "github.com/fbaube/xmlmodels"
 )
 
-/*
-func (f *FlagSet) StringVarP(p *string, name, shorthand string, value string, usage string)
-func (f *FlagSet) BoolVarP(p *bool, name, shorthand string, value bool, usage string)
-func (f *FlagSet) IntVarP(p *int, name, shorthand string, value int, usage string)
-*/
-
-var sOutRelFP, sXmlCatRelFP, sXmlCatSearchRelFP string
+var inArg, outArg, dbArg, xmlCatArg, xmlSchemasArg string
 
 // XmlAppConfiguration can probably be used with various 3rd-party utilities.
 type XmlAppConfiguration struct {
-	AppName                       string
-	DBdirPath                     string
-	DBhandle                      *db.MmmcDB
-	In, Out, XmlCat, XmlCatSearch FU.PathInfo // NOT ptr! Barfs at startup.
-	RestPort                      int
+	AppName                                           string
+	DBhandle                                          *db.MmmcDB
+	Infile, Outfile, Dbdir, Xmlcatfile, Xmlschemasdir FU.PathInfo // NOT ptr! Barfs at startup.
+	RestPort                                          int
 	// CLI flags
 	FollowSymLinks, Pritt, DBdoImport, Help, Debug, GroupGenerated, Validate, DBdoZeroOut bool
 	// Result of processing CLI arg for input file(s)
 	SingleFile bool
 	// Result of processing CLI args (-c, -s)
-	*gparse.XmlCatalogRecord
+	*XM.XmlCatalogFile
 }
 
-// var InFP, OutFP, XmlCatFP, XmlCatSearchFP string
-var xmlCatalogRecords []*gparse.XmlCatalogRecord
+var myAppName string
+
+var multipleXmlCatalogFiles []*XM.XmlCatalogFile
 
 // CA maybe should not be exported. Or should be generated
 // on-the-fly instead of being a Singleton.
-var CA XmlAppConfiguration
+// // var CA XmlAppConfiguration
 
 func myUsage() {
 	//  Println(CA.AppName, "[-d] [-g] [-h] [-m] [-p] [-v] [-z] [-D] [-o outfile] [-d dbdir] Infile")
-	fmt.Println(CA.AppName, "[-d] [-g] [-h] [-m] [-p] [-v] [-z] [-D] [-d dbdir] [-r port] Infile")
-	fmt.Println("   Process mixed content XML, XHTML (XDITA), and Markdown (MDITA) input.")
+	fmt.Println(myAppName, "[-d] [-g] [-h] [-m] [-p] [-v] [-z] [-D] [-d dbdir] [-r port] Infile")
+	fmt.Println("   Process mixed content XML, XHTML/XDITA, and Markdown/MDITA input.")
 	fmt.Println("   Infile is a single file or directory name; no wildcards (?,*).")
 	fmt.Println("          If a directory, it is processed recursively.")
 	fmt.Println("   Infile may be \"-\" for Stdin: input typed (or pasted) interactively")
 	fmt.Println("          is written to file ./Stdin.xml for processing")
 	flag.Usage()
 }
-
-// What are the filepaths we can ask for ?
-// input file(s)/dir
-// output file(s)/dir
-// path to XML catalog file
-// path to schema file(s)
-// path to DB file
 
 // For each DOCTYPE. also an XSL, thus XSL catalog file.
 // Maybe also same for CSS.
@@ -83,34 +68,33 @@ var UMM = map[string]string{
 		"(e.g. ./Filnam.xml maps to ./Filenam.xml_gxml/Filenam.*)",
 	"m": "Import input file(s) to database",
 	"p": "Pretty-print to file with \"fmtd-\" prepended to file extension",
-	"v": "Validate input file(s)? (using xmllint) (with flag \"-c\" or \"-s\")",
+	"v": "Validate input file(s) (using xmllint) (with flag \"-c\" or \"-s\")",
 	"z": "Zero out the database",
 	"D": "Turn on debugging",
 	"L": "Follow symbolic links in directory recursion",
 	// All others
 	"c": "XML `catalog_filepath` (do not use with \"-s\" flag)",
-	"d": "Directory path of/for database mmmc.db",
-	"r": "Run REST server on `port number`",
-	"s": "Directory `path` to DTD schema file(s) (.dtd, .mod)",
+	"o": "`output_filepath` (possibly ignored, depending on command)",
+	"d": "Database mmmc.db `directory_path`",
+	"r": "Run REST server on `port_number`",
+	"s": "DTD schema file(s) `directory_path` (.dtd, .mod)",
 }
 
-func initVars() {
-	// flag.StringVar(&CA.Out.RelFilePath, "o", "", // &CA.outArg, "o", "",
-	// 	"Output file name (possibly ignored, depending on command)")
-	// pflag:
-	// flag.BoolVarP(&flagvar, "boolname", "b", true, "help message")
-	flag.StringVarP(&sXmlCatRelFP, "catalog", "c", "", UMM["c"])
-	flag.StringVarP(&CA.DBdirPath, "db-dir", "d", "", UMM["d"])
-	flag.StringVarP(&sXmlCatSearchRelFP, "search", "s", "", UMM["s"])
-	flag.BoolVarP(&CA.DBdoImport, "import", "m", false, UMM["m"])
-	flag.BoolVarP(&CA.FollowSymLinks, "symlinks", "L", true, UMM["L"])
-	flag.BoolVarP(&CA.GroupGenerated, "group-gen", "g", false, UMM["g"])
-	flag.BoolVarP(&CA.Pritt, "pretty", "p", true, UMM["p"])
-	flag.BoolVarP(&CA.Debug, "debug", "D", false, UMM["D"])
-	flag.BoolVarP(&CA.Help, "help", "h", false, UMM["h"])
-	flag.BoolVarP(&CA.Validate, "validate", "v", false, UMM["v"])
-	flag.BoolVarP(&CA.DBdoZeroOut, "zero-out", "z", false, UMM["z"])
-	flag.IntVarP(&CA.RestPort, "rest-port", "r", 0, UMM["r"])
+func initVars(pXAC *XmlAppConfiguration) {
+	// f.StringVarP(&inArg, "infile", "i", "", UMM["i"])
+	flag.StringVarP(&outArg, "outfile", "o", "", UMM["o"])
+	flag.StringVarP(&xmlCatArg, "catalog", "c", "", UMM["c"])
+	flag.StringVarP(&dbArg, "db-dir", "d", "", UMM["d"])
+	flag.StringVarP(&xmlSchemasArg, "search", "s", "", UMM["s"])
+	flag.BoolVarP(&pXAC.DBdoImport, "import", "m", false, UMM["m"])
+	flag.BoolVarP(&pXAC.FollowSymLinks, "symlinks", "L", true, UMM["L"])
+	flag.BoolVarP(&pXAC.GroupGenerated, "group-gen", "g", false, UMM["g"])
+	flag.BoolVarP(&pXAC.Pritt, "pretty", "p", true, UMM["p"])
+	flag.BoolVarP(&pXAC.Debug, "debug", "D", false, UMM["D"])
+	flag.BoolVarP(&pXAC.Help, "help", "h", false, UMM["h"])
+	flag.BoolVarP(&pXAC.Validate, "validate", "v", false, UMM["v"])
+	flag.BoolVarP(&pXAC.DBdoZeroOut, "zero-out", "z", false, UMM["z"])
+	flag.IntVarP(&pXAC.RestPort, "rest-port", "r", 0, UMM["r"])
 	EnableAllFlags()
 	// fmt.Printf("FLAGS %+v \n", flag.CommandLine)
 	// func (f *FlagSet) VisitAll(fn func(*Flag))
@@ -123,20 +107,20 @@ func checkbarf(e error, s string) {
 	if e == nil {
 		return
 	}
-	MU.SessionLogger.Printf("%s failed: %s \n", CA.AppName, e)
-	fmt.Fprintf(os.Stderr, "%s failed: %s \n", CA.AppName, e)
+	MU.SessionLogger.Printf("%s failed: %s \n", myAppName, e)
+	fmt.Fprintf(os.Stderr, "%s failed: %s \n", myAppName, e)
 	MU.ErrorTrace(os.Stderr, e)
 	os.Exit(1)
 }
 
-// ProcessArgs wants to be a generic CLI arguments for any XML-related command.
-// If this is to be so, there should be a way to selectively disable commands
-// that are inappropriate for the CLI command it is being integrated into.
-func ProcessArgs(appName string, osArgs []string) (*XmlAppConfiguration, error) {
-	initVars()
+// NewXmlAppConfiguration processes CLI arguments for any XML-related command.
+func NewXmlAppConfiguration(appName string, osArgs []string) (*XmlAppConfiguration, error) {
+	var pXAC *XmlAppConfiguration
+	initVars(pXAC)
 	DisableFlags("hDgpr")
 	// Do not use logging until the invocation is sorted out.
-	CA.AppName = appName
+	myAppName = appName
+	pXAC.AppName = appName
 	var e error
 
 	if !WU.IsWasm() {
@@ -148,7 +132,7 @@ func ProcessArgs(appName string, osArgs []string) (*XmlAppConfiguration, error) 
 		xl, e := exec.LookPath("xmllint")
 		if e != nil {
 			xl = "not found"
-			if CA.Validate {
+			if pXAC.Validate {
 				println("==> Validation is not possible: xmllint cannot be found")
 			}
 		}
@@ -156,18 +140,19 @@ func ProcessArgs(appName string, osArgs []string) (*XmlAppConfiguration, error) 
 	}
 	// == Examine CLI invocation flags ==
 	flag.Parse()
+	fmt.Printf("CMD-TAIL: %+v \n", flag.Args())
 	// FIXME - pos'l arg OR "-i" OR stdin OR "-"
 	if len(osArgs) < 2 || nil == flag.Args() || 0 == len(flag.Args()) {
 		println("==> Argument parsing failed. Did not specify input file(s)?")
 		myUsage()
 		os.Exit(1)
 	}
-	if CA.Debug {
-		fmt.Printf("D=> Flags: debug:%s groupGen:%s help:%s "+
+	if pXAC.Debug {
+		fmt.Printf("D=> Flags: debug:%s grpGen:%s help:%s "+
 			"import:%s printty:%s validate:%s zeroOutDB:%s restPort:%d \n",
-			SU.Yn(CA.Debug), SU.Yn(CA.GroupGenerated), // d g h m p v z r
-			SU.Yn(CA.Help), SU.Yn(CA.DBdoImport), SU.Yn(CA.Pritt),
-			SU.Yn(CA.Validate), SU.Yn(CA.DBdoZeroOut), CA.RestPort)
+			SU.Yn(pXAC.Debug), SU.Yn(pXAC.GroupGenerated), // d g h m p v z r
+			SU.Yn(pXAC.Help), SU.Yn(pXAC.DBdoImport), SU.Yn(pXAC.Pritt),
+			SU.Yn(pXAC.Validate), SU.Yn(pXAC.DBdoZeroOut), pXAC.RestPort)
 		fmt.Println("D=> CLI tail:", flag.Args())
 	}
 
@@ -178,7 +163,7 @@ func ProcessArgs(appName string, osArgs []string) (*XmlAppConfiguration, error) 
 	// Handle case where XML comes from standard input i.e. os.Stdin
 	if flag.Args()[0] == "-" {
 		if WU.IsWasm() {
-			println("==> Trying to read from Stdin; press ^D right after a newline to end")
+			println("==> FIXME Trying to read from Stdin; press ^D right after a newline to end")
 		} else {
 			stat, e := os.Stdin.Stat()
 			checkbarf(e, "Cannot Stat() Stdin")
@@ -193,7 +178,7 @@ func ProcessArgs(appName string, osArgs []string) (*XmlAppConfiguration, error) 
 		checkbarf(e, "Cannot read from Stdin")
 		e = ioutil.WriteFile("Stdin.xml", []byte(stdIn), 0666)
 		checkbarf(e, "Cannot write to ./Stdin.xml")
-		CA.In = *FU.NewPathInfo("Stdin.xml") // .RelFilePath = "Stdin.xml"
+		pXAC.Infile = *FU.NewPathInfo("Stdin.xml") // .RelFilePath = "Stdin.xml"
 
 	} else {
 		// ===========================================
@@ -201,19 +186,19 @@ func ProcessArgs(appName string, osArgs []string) (*XmlAppConfiguration, error) 
 		//   info about path, existence, and type
 		// ===========================================
 		// Process input-file(s) argument, which can be a relative filepath.
-		CA.In = *FU.NewPathInfo(flag.Args()[0])
+		pXAC.Infile = *FU.NewPathInfo(flag.Args()[0])
 		// If the absolute path does not match the argument provided, inform the user.
-		if CA.In.AbsFP() != flag.Args()[0] { // CA.In.RelFilePath { // CA.In.ArgFilePath {
-			println("==> Input:", FU.Enhomed(CA.In.AbsFP()))
+		if pXAC.Infile.AbsFP() != flag.Args()[0] { // CA.In.RelFilePath { // CA.In.ArgFilePath {
+			println("==> Input:", FU.Enhomed(pXAC.Infile.AbsFP()))
 		}
-		if CA.In.IsOkayDir() {
+		if pXAC.Infile.IsOkayDir() {
 			println("    --> The input is a directory and will be processed recursively.")
-		} else if CA.In.IsOkayFile() {
+		} else if pXAC.Infile.IsOkayFile() {
 			println("    --> The input is a single file: extra info will be listed here.")
-			CA.SingleFile = true
+			pXAC.SingleFile = true
 		} else {
 			println("    --> The input is a type not understood.")
-			return nil, fmt.Errorf("Bad type for input: " + CA.In.AbsFP())
+			return nil, fmt.Errorf("Bad type for input: " + pXAC.Infile.AbsFP())
 		}
 	}
 
@@ -224,55 +209,54 @@ func ProcessArgs(appName string, osArgs []string) (*XmlAppConfiguration, error) 
 
 	// Process output-file(s) argument, which can be a relative filepath.
 	// CA.Out.ProcessFilePathArg(CA.Out.ArgFilePath)
-	CA.Out = *FU.NewPathInfo(sOutRelFP) // CA.Out.RelFilePath)
+	pXAC.Outfile = *FU.NewPathInfo(outArg) // CA.Out.RelFilePath)
 
 	// Process database directory argument, which can be a relative filepath.
 	// CA.DB.ProcessFilePathArg(CA.DBdirPath)
 
 	// ====
 
-	pCA := &CA
-	e = pCA.ProcessDatabaseArgs()
+	e = pXAC.ProcessDatabaseArgs()
 	checkbarf(e, "Could not process DB directory argument(s)")
-	e = pCA.ProcessCatalogArgs()
+	e = pXAC.ProcessCatalogArgs()
 	checkbarf(e, "Could not process XML catalog argument(s)")
-	return pCA, e
+	return pXAC, e
 }
 
-func (pCA *XmlAppConfiguration) ProcessDatabaseArgs() error {
+func (pXAC *XmlAppConfiguration) ProcessDatabaseArgs() error {
 	var mustAccessTheDB, theDBexists bool
 	var e error
-	mustAccessTheDB = pCA.DBdoImport || pCA.DBdoZeroOut || pCA.DBdirPath != ""
+	mustAccessTheDB = pXAC.DBdoImport || pXAC.DBdoZeroOut || dbArg != ""
 	if !mustAccessTheDB {
 		return nil
 	}
-	pCA.DBhandle, e = db.NewMmmcDB(pCA.DBdirPath)
+	pXAC.DBhandle, e = db.NewMmmcDB(dbArg)
 	if e != nil {
 		return fmt.Errorf("DB setup failure: %w", e)
 	}
-	theDBexists = CA.DBhandle.PathInfo.Exists()
+	theDBexists = pXAC.DBhandle.PathInfo.Exists()
 	var s = "exists"
 	if !theDBexists {
 		s = "does not exist"
 	}
-	fmt.Printf("==> DB %s: %s\n", s, pCA.DBhandle.PathInfo.AbsFP())
+	fmt.Printf("==> DB %s: %s\n", s, pXAC.DBhandle.PathInfo.AbsFP())
 
-	if pCA.DBdoZeroOut {
+	if pXAC.DBdoZeroOut {
 		println("    --> Zeroing out DB")
-		pCA.DBhandle.MoveCurrentToBackup()
-		pCA.DBhandle.ForceEmpty()
+		pXAC.DBhandle.MoveCurrentToBackup()
+		pXAC.DBhandle.ForceEmpty()
 	} else {
-		pCA.DBhandle.DupeCurrentToBackup()
-		pCA.DBhandle.ForceExistDBandTables()
+		pXAC.DBhandle.DupeCurrentToBackup()
+		pXAC.DBhandle.ForceExistDBandTables()
 	}
 	// spew.Dump(pCA.DBhandle)
 	return nil
 }
 
-func (pCA *XmlAppConfiguration) ProcessCatalogArgs() error {
+func (pXAC *XmlAppConfiguration) ProcessCatalogArgs() error {
 	var gotC, gotS bool
-	gotC = ("" != sXmlCatRelFP)
-	gotS = ("" != sXmlCatSearchRelFP)
+	gotC = ("" != xmlCatArg)
+	gotS = ("" != xmlSchemasArg)
 	if !(gotC || gotS) {
 		return nil
 	}
@@ -281,36 +265,36 @@ func (pCA *XmlAppConfiguration) ProcessCatalogArgs() error {
 	}
 	if gotC { // -c
 		// pCA.XmlCat.ProcessFilePathArg(CA.XmlCat.ArgFilePath)
-		CA.XmlCat = *FU.NewPathInfo(sXmlCatRelFP)
-		if !(pCA.XmlCat.IsOkayFile() && pCA.XmlCat.Size() > 0) {
-			println("==> ERROR: XML catalog filepath is not file: " + pCA.XmlCat.AbsFP())
+		pXAC.Xmlcatfile = *FU.NewPathInfo(xmlCatArg)
+		if !(pXAC.Xmlcatfile.IsOkayFile() && pXAC.Xmlcatfile.Size() > 0) {
+			println("==> ERROR: XML catalog filepath is not file: " + pXAC.Xmlcatfile.AbsFP())
 			return errors.New(fmt.Sprintf("mcfile.ConfArgs.ProcCatalArgs<%s:%s>",
-				sXmlCatRelFP, CA.XmlCat.AbsFP()))
+				xmlCatArg, pXAC.Xmlcatfile.AbsFP()))
 		}
-		println("==> Catalog:", sXmlCatRelFP)
-		if pCA.XmlCat.AbsFP() != sXmlCatRelFP {
-			println("     --> i.e. ", FU.Enhomed(pCA.XmlCat.AbsFP()))
+		println("==> Catalog:", xmlCatArg)
+		if pXAC.Xmlcatfile.AbsFP() != xmlCatArg {
+			println("     --> i.e. ", FU.Enhomed(pXAC.Xmlcatfile.AbsFP()))
 		}
 	}
 	if gotS { // -s
-		pCA.XmlCatSearch = *FU.NewPathInfo(sXmlCatSearchRelFP)
-		if !pCA.XmlCatSearch.IsOkayDir() {
+		pXAC.Xmlschemasdir = *FU.NewPathInfo(xmlSchemasArg)
+		if !pXAC.Xmlschemasdir.IsOkayDir() {
 			return errors.New("mcfile.ConfArgs.ProcCatalArgs: cannot open XML catalog directory: " +
-				pCA.XmlCatSearch.AbsFP())
+				pXAC.Xmlschemasdir.AbsFP())
 		}
 	}
 	var e error
 	if gotS { // -s and not -c
-		println("==> Schema(s):", sXmlCatSearchRelFP)
-		pCA.XmlCatSearch = *FU.NewPathInfo(sXmlCatSearchRelFP)
-		if CA.XmlCatSearch.AbsFP() != sXmlCatSearchRelFP {
-			println("     --> i.e. ", FU.Enhomed(pCA.XmlCatSearch.AbsFP()))
+		println("==> Schema(s):", xmlSchemasArg)
+		pXAC.Xmlschemasdir = *FU.NewPathInfo(xmlSchemasArg)
+		if pXAC.Xmlschemasdir.AbsFP() != xmlSchemasArg {
+			println("     --> i.e. ", FU.Enhomed(pXAC.Xmlschemasdir.AbsFP()))
 		}
-		if !pCA.XmlCatSearch.IsOkayDir() {
+		if !pXAC.Xmlschemasdir.IsOkayDir() {
 			println("==> ERROR: Schema path is not a readable directory: " +
-				FU.Enhomed(pCA.XmlCatSearch.AbsFP()))
+				FU.Enhomed(pXAC.Xmlschemasdir.AbsFP()))
 			return fmt.Errorf("mcfile.ConfArgs.ProcCatalArgs.abs<%s>: %w",
-				pCA.XmlCatSearch.AbsFP(), e)
+				pXAC.Xmlschemasdir.AbsFP(), e)
 		}
 	}
 	// println(" ")
@@ -321,30 +305,30 @@ func (pCA *XmlAppConfiguration) ProcessCatalogArgs() error {
 
 	// IF user asked for a single catalog file
 	if gotC && !gotS {
-		CA.XmlCatalogRecord, e = gparse.NewXmlCatalogRecordFromFile(sXmlCatRelFP)
+		pXAC.XmlCatalogFile, e = XM.NewXmlCatalogFile_noGT(xmlCatArg)
 		if e != nil {
-			println("==> ERROR: Can't find or process catalog file:", sXmlCatRelFP)
+			println("==> ERROR: Can't find or process catalog file:", xmlCatArg)
 			println("    Error was:", e.Error())
-			CA.XmlCatalogRecord = nil
-			return fmt.Errorf("gxml.Confargs.NewXmlCatalogFromFile<%s>: %w", sXmlCatRelFP, e)
+			pXAC.XmlCatalogFile = nil
+			return fmt.Errorf("gxml.Confargs.NewXmlCatalogFromFile<%s>: %w", xmlCatArg, e)
 		}
-		if CA.XmlCatalogRecord == nil ||
-			len(CA.XmlCatalogRecord.XmlPublicIDsubrecords) == 0 {
-			println("==> No valid entries in catalog file:", sXmlCatRelFP)
-			CA.XmlCatalogRecord = nil
+		if pXAC.XmlCatalogFile == nil ||
+			len(pXAC.XmlCatalogFile.XmlPublicIDsubrecords) == 0 {
+			println("==> No valid entries in catalog file:", xmlCatArg)
+			pXAC.XmlCatalogFile = nil
 		}
 		return nil
 	}
 	// IF user asked for a directory scan of schema files
 	if gotS && !gotC {
-		xmlCatalogRecords = make([]*gparse.XmlCatalogRecord, 0)
+		multipleXmlCatalogFiles = make([]*XM.XmlCatalogFile, 0)
 		fileNameToUse := "catalog.xml"
-		if sXmlCatRelFP != "" {
-			fileNameToUse = sXmlCatRelFP
+		if xmlCatArg != "" {
+			fileNameToUse = xmlCatArg
 		}
 		filePathToUse := FU.AbsFilePath(".")
-		if sXmlCatRelFP != "" {
-			filePathToUse = FU.AbsFilePath(CA.XmlCatSearch.AbsFP())
+		if xmlCatArg != "" {
+			filePathToUse = FU.AbsFilePath(pXAC.Xmlschemasdir.AbsFP())
 		}
 		fileNameList, e := filePathToUse.GatherNamedFiles(fileNameToUse)
 		if e != nil {
@@ -356,8 +340,8 @@ func (pCA *XmlAppConfiguration) ProcessCatalogArgs() error {
 		}
 		// For every catalog file (usually just one)
 		for _, filePathToUse = range fileNameList {
-			var xmlCat *gparse.XmlCatalogRecord
-			xmlCat, e = gparse.NewXmlCatalogRecordFromFile(filePathToUse.S())
+			var xmlCat *XM.XmlCatalogFile
+			xmlCat, e = XM.NewXmlCatalogFile_noGT(filePathToUse.S())
 			if e != nil {
 				println("==> ERROR: Can't find or process catalog file:", filePathToUse)
 				println("    Error was:", e.Error())
@@ -367,41 +351,41 @@ func (pCA *XmlAppConfiguration) ProcessCatalogArgs() error {
 				println("==> No valid entries in catalog file:", filePathToUse)
 				continue
 			}
-			xmlCatalogRecords = append(xmlCatalogRecords, xmlCat)
+			multipleXmlCatalogFiles = append(multipleXmlCatalogFiles, xmlCat)
 		}
-		switch len(xmlCatalogRecords) {
+		switch len(multipleXmlCatalogFiles) {
 		case 0:
 			fmt.Printf("==> ERROR: No files named <%s> found in+under <%s>:",
 				fileNameToUse, filePathToUse)
-			CA.XmlCatalogRecord = nil
+			pXAC.XmlCatalogFile = nil
 			return fmt.Errorf("gxml.Confargs.XmlCatalogs<%s:%s>: %w",
 				fileNameToUse, filePathToUse, e)
 		case 1:
-			CA.XmlCatalogRecord = xmlCatalogRecords[0]
+			pXAC.XmlCatalogFile = multipleXmlCatalogFiles[0]
 		default:
 			// MERGE THEM ALL
-			var xmlCat *gparse.XmlCatalogRecord
-			CA.XmlCatalogRecord = new(gparse.XmlCatalogRecord)
-			CA.XmlCatalogRecord.XmlPublicIDsubrecords =
+			var xmlCat *XM.XmlCatalogFile
+			pXAC.XmlCatalogFile = new(XM.XmlCatalogFile)
+			pXAC.XmlCatalogFile.XmlPublicIDsubrecords =
 				make([]XM.PIDSIDcatalogFileRecord, 0)
-			for _, xmlCat = range xmlCatalogRecords {
-				CA.XmlCatalogRecord.XmlPublicIDsubrecords =
-					append(CA.XmlCatalogRecord.XmlPublicIDsubrecords,
+			for _, xmlCat = range multipleXmlCatalogFiles {
+				pXAC.XmlCatalogFile.XmlPublicIDsubrecords =
+					append(pXAC.XmlCatalogFile.XmlPublicIDsubrecords,
 						xmlCat.XmlPublicIDsubrecords...)
 			}
 		}
 	}
-	if CA.XmlCatalogRecord == nil ||
-		CA.XmlCatalogRecord.XmlPublicIDsubrecords == nil ||
-		len(CA.XmlCatalogRecord.XmlPublicIDsubrecords) == 0 {
-		CA.XmlCatalogRecord = nil
+	if pXAC.XmlCatalogFile == nil ||
+		pXAC.XmlCatalogFile.XmlPublicIDsubrecords == nil ||
+		len(pXAC.XmlCatalogFile.XmlPublicIDsubrecords) == 0 {
+		pXAC.XmlCatalogFile = nil
 		println("==> No valid catalog entries")
 		return errors.New("gxml.Confargs.XmlCatalogs")
 	}
 	// println("==> Contents of XML catalog(s):")
 	// print(CA.XmlCatalog.DString())
 	fmt.Printf("==> XML catalog(s) yielded %d valid entries \n",
-		len(CA.XmlCatalogRecord.XmlPublicIDsubrecords))
+		len(pXAC.XmlCatalogFile.XmlPublicIDsubrecords))
 
 	// TODO:470 If import, create batch info ?
 	return nil
