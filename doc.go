f8a052b7f58b4d9d3e9527c3e465bbc72c78f9f9
Fred Baube <fbaube@welho.com>
2021-04-30 15:01:11 +0300

    Initial.

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Package cliflagutils provides a generic command line argument processing
+// capability for XML-oriented programs. It provides a large set of useful
+// arguments that can individually (or as a group) be enabled or else
+// disabled (i.e. hidden).
+package cliflagutils
